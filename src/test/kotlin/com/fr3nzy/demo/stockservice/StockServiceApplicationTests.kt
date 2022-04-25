b119fe81a4ae5032d127bb996b8a5c96b241ab1a
adzhukov <adzhukov@inno.tech>
2022-04-25 15:14:41 +0300

    init

---
+++ src/test/kotlin/com/fr3nzy/demo/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fr3nzy.demo.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
